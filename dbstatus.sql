4debc921aaa93777c1ffaaa0cb47663d9f97f658
hemanth22 <hemanth22hemu@gmail.com>
2019-07-31 10:02:03 +0530

    closes #3 visit change log.

--- dbstatus.sql
+++ dbstatus.sql
@@ -1,1 +1,3 @@
 select database_status from v$instance;
+create user scott identified by tiger quota 100M on users
+GRANT CONNECT,RESOURCE,UNLIMITED TABLESPACE TO SCOTT IDENTIFIED BY TIGER;
--- unlockscott.sql
+++ unlockscott.sql
@@ -1,3 +1,2 @@
 connect system/oracle
-create user scott identified by tiger quota 100M on users
 @scott.sql
