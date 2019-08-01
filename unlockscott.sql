0978f38bc59d06d8acc990d70b6e35639eb276fc
Hemanth B <hemanth22hemu@gmail.com>
2019-08-01 10:59:13 +0530

    Update unlockscott.sql

--- unlockscott.sql
+++ unlockscott.sql
@@ -1,4 +1,3 @@
 connect SYS/oracle@//localhost:1521/xe as sysdba
-create user scott identified by tiger quota 100M on users;
 GRANT CONNECT,RESOURCE,UNLIMITED TABLESPACE TO SCOTT IDENTIFIED BY TIGER;
 select * from ALL_USERS where USERNAME='SCOTT';
